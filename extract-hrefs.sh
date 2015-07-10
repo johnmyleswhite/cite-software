7c4a8500f8e9b3fdd156e8549e40e2bb01b23a25
Ryan Curtin <ryan@ratml.org>
2015-07-10 15:57:52 +0000

    Hacky bash script that does an okay job of extracting URLs.

---
+++ extract-hrefs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Hacktastic piece of crap script to pull out hrefs from files.
+# Usage:
+#   extract-hrefs.sh input_file.html
+# Output to stdout.
+
+if [ "a$1" == "a" ]; then
+  echo "Argument required: input file"
+  exit 1;
+fi
+
+# Get the easy stuff that's contained in <a href="...">.
+cat $1 | grep -a '<A href=\"' | sed -E 's/.*<A href=\"([^"]*)\".*/\1 /g' | grep -a -v $1
